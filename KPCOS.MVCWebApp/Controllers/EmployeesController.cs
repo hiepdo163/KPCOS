de9dc09afc7d14400d7aedab07af95039cf0a827
it-is-feasible <hiepmap1579@gmail.com>
2024-10-17 23:29:04 +0700

    add employee mvc

---
+++ KPCOS.MVCWebApp/Controllers/EmployeesController.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KPCOS.Data.Models;
+using KPCOS.Common;
+using KPCOS.Service.Base;
+using Newtonsoft.Json;
+using KPCOS.Service.DTOs;
+
+namespace KPCOS.MVCWebApp.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: Employees
+        public async Task<IActionResult> Index()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(Const.APIEndpoint + "Employee"))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+
+                        if (result != null && result.Data != null)
+                        {
+                            var data = JsonConvert.DeserializeObject<List<Employee>>(result.Data.ToString());
+                            return View(data);
+                        }
+                    }
+                }
+            }
+            return View(new List<Employee>());
+            //return View();
+        }
+
+        // GET: Employees/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(Const.APIEndpoint + "Employee/" + id))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+
+                        if (result != null && result.Data != null)
+                        {
+                            var data = JsonConvert.DeserializeObject<Employee>(result.Data.ToString());
+                            return View(data);
+                        }
+                    }
+                }
+            }
+            return View(new Employee());
+        }
+
+        // GET: Employees/Create
+        public async Task<IActionResult> Create()
+        {
+            ViewData["UserId"] = new SelectList(await this.GetUsers(), "Id", "Fullname");
+            ViewData["SupervisorId"] = new SelectList(await this.GetSupervisors(), "Id", "User.Fullname");
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(EmployeeDTO employee)
+        {
+            bool Status = false;
+            if (ModelState.IsValid)
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    using (var response = await httpClient.PostAsJsonAsync(Const.APIEndpoint + "Employee", employee))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var content = await response.Content.ReadAsStringAsync();
+                            var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+
+                            if (result != null && result.Status == Const.SUCCESS_CREATE_CODE)
+                            {
+                                Status = true;
+                            }
+                            else
+                            {
+                                //return View(customer);
+                                Status = false;
+                            }
+                        }
+                    }
+                }
+            }
+            if (Status)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                ViewData["UserId"] = new SelectList(await this.GetUsers(), "Id", "Fullname", employee.UserId);
+                ViewData["SupervisorId"] = new SelectList(await this.GetSupervisors(), "Id", "User.Fullname", employee.SupervisorId);
+                return View(employee);
+            }
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            var employee = new Employee();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(Const.APIEndpoint + "Employee/" + id))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+
+                        if (result != null && result.Data != null)
+                        {
+                            employee = JsonConvert.DeserializeObject<Employee>(result.Data.ToString());
+                        }
+                    }
+                }
+            }
+            ViewData["UserId"] = new SelectList(await this.GetUsers(), "Id", "Fullname", employee.UserId);
+            ViewData["SupervisorId"] = new SelectList(await this.GetSupervisors(), "Id", "User.Fullname", employee.SupervisorId);
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, EmployeeDTO employee)
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    using (var response = await httpClient.PutAsJsonAsync(Const.APIEndpoint + "Employee/"+ id, employee))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var content = await response.Content.ReadAsStringAsync();
+                            var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+
+                            if (result != null && result.Status == Const.SUCCESS_UPDATE_CODE)
+                            {
+                                status = true;
+                            }
+                            else
+                            {
+                                //return View(customer);
+                                status = false;
+                            }
+                        }
+                    }
+                }
+            }
+            if (status)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                ViewData["UserId"] = new SelectList(await this.GetUsers(), "Id", "Fullname", employee.UserId);
+                ViewData["SupervisorId"] = new SelectList(await this.GetSupervisors(), "Id", "User.Fullname", employee.SupervisorId);
+                return View(employee);
+            }
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(Const.APIEndpoint + "Employee/" + id))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+
+                        if (result != null && result.Data != null)
+                        {
+                            var data = JsonConvert.DeserializeObject<Employee>(result.Data.ToString());
+                            return View(data);
+                        }
+                    }
+                }
+            }
+            return View(new Employee());
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    using (var response = await httpClient.DeleteAsync(Const.APIEndpoint + "Employee/" + id))
+                    {
+                        if (response.IsSuccessStatusCode)
+                        {
+                            var content = await response.Content.ReadAsStringAsync();
+                            var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+
+                            if (result != null && result.Status == Const.SUCCESS_DELETE_CODE)
+                            {
+                                status = true;
+                            }
+                            else
+                            {
+                                //return View(customer);
+                                status = false;
+                            }
+                        }
+                    }
+                }
+            }
+            if (status)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return RedirectToAction(nameof(Delete));
+            }
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            var users = new List<User>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(Const.APIEndpoint + "User/get-all-by-role/employee"))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+
+                        if (result != null && result.Data != null)
+                        {
+                            users = JsonConvert.DeserializeObject<List<User>>(result.Data.ToString());
+                        }
+                    }
+                }
+            }
+            return users;
+        }
+        public async Task<List<Employee>> GetSupervisors()
+        {
+            var employees = new List<Employee>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(Const.APIEndpoint + "Employee"))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        var result = JsonConvert.DeserializeObject<BusinessResult>(content);
+
+                        if (result != null && result.Data != null)
+                        {
+                            employees = JsonConvert.DeserializeObject<List<Employee>>(result.Data.ToString());
+                        }
+                    }
+                }
+            }
+            return employees;
+        }
+    }
+}
